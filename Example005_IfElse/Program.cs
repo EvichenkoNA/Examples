a495f80005b26e6d4dedf3d6669633ab1a3f844a
Natalia <natalia.evichenko@mail.ru>
2022-12-01 16:34:11 +0300

    Add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Ура, это же Наташа!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
