c0e06c39a12d89b0d02f7ae954008035c5e939de
Natalia <natalia.evichenko@mail.ru>
2022-12-05 00:35:15 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 52, 5, 83, 34, 5, 96, 17, 38 };
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        //break;                              //Прервать, т.к. несколько одтнаковых значений в массиве
+    }
+    index++;
+}
